9256fe75e838d5b04cd475341bba7954dacbd0e5
Dallas98 <990259227@qq.com>
2019-12-31 14:55:05 +0800

    add publish page

---
+++ src/main/java/com/dallas/community/controller/PublishController.java
@@ -0,0 +1,10 @@
+package com.dallas.community.controller;
+
+/**
+ * @program: community
+ * @description:
+ * @author: Dallas
+ * @create: 2019-12-31 13:35
+ */
+public class PublishController {
+}
