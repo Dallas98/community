4e4a896bd2c59be14d72f31905e9b4b8ed17fb58
Dallas98 <990259227@qq.com>
2019-12-31 16:42:42 +0800

    complete publish function

---
+++ src/main/java/com/dallas/community/mapper/QuestionMapper.java
@@ -0,0 +1,18 @@
+package com.dallas.community.mapper;
+
+import com.dallas.community.model.Question;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @program: community
+ * @description:
+ * @author: Dallas
+ * @create: 2019-12-31 15:10
+ */
+
+@Mapper
+public interface QuestionMapper {
+    @Insert("insert into question (title,description,gmt_create,gmt_modified,creator,tag) values (#{title},#{description},#{gmt_create},#{gmt_modified},#{creator},#{tag})")
+    void create(Question question);
+}
