b451ea616d036b377bfc6025bdf82ebac196405e
Dallas98 <990259227@qq.com>
2020-01-05 14:47:26 +0800

    add Lombok

--- src/main/java/com/dallas/community/dto/GithubUser.java
+++ src/main/java/com/dallas/community/dto/GithubUser.java
@@ -1,55 +1,18 @@
 package com.dallas.community.dto;
 
+import lombok.Data;
+
 /**
  * @program: community
  * @description:
  * @author: Dallas
  * @create: 2019-12-28 13:12
  */
+
+@Data
 public class GithubUser {
     private String name;
     private long id;
     private String bio;
     private String avatar_url;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getBio() {
-        return bio;
-    }
-
-    public void setBio(String bio) {
-        this.bio = bio;
-    }
-
-    public String getAvatar_url() {
-        return avatar_url;
-    }
-
-    public void setAvatar_url(String avatar_url) {
-        this.avatar_url = avatar_url;
-    }
-
-    @Override
-    public String toString() {
-        return "GithubUser{" +
-                "name='" + name + '\'' +
-                ", id=" + id +
-                ", bio='" + bio + '\'' +
-                '}';
-    }
 }
--- src/main/java/com/dallas/community/model/User.java
+++ src/main/java/com/dallas/community/model/User.java
@@ -1,11 +1,15 @@
 package com.dallas.community.model;
 
+import lombok.Data;
+
 /**
  * @program: community
  * @description:
  * @author: Dallas
  * @create: 2019-12-29 16:53
  */
+
+@Data
 public class User {
     private Integer id;
     private String account_id;
@@ -14,60 +18,4 @@ public class User {
     private Long gmt_create;
     private Long gmt_modified;
     private String avatar_url;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getAccount_id() {
-        return account_id;
-    }
-
-    public void setAccount_id(String account_id) {
-        this.account_id = account_id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public Long getGmt_create() {
-        return gmt_create;
-    }
-
-    public void setGmt_create(Long gmt_create) {
-        this.gmt_create = gmt_create;
-    }
-
-    public Long getGmt_modified() {
-        return gmt_modified;
-    }
-
-    public void setGmt_modified(Long gmt_modified) {
-        this.gmt_modified = gmt_modified;
-    }
-
-    public String getAvater_url() {
-        return avatar_url;
-    }
-
-    public void setAvater_url(String avatar_url) {
-        this.avatar_url = avatar_url;
-    }
 }
